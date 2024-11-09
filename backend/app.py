10328abff8b41bbc60b6f1a5b3253c750070da8f
Yazan Zidan <yazan.zidan@maf.ae>
2024-11-09 19:35:05 +0300

    adding host for testing

--- backend/app.py
+++ backend/app.py
@@ -7,4 +7,4 @@ def api():
     return jsonify(message="Hello from the BACKEND :)"), 200
 
 if __name__ == '__main__':
-    app.run(host='44.206.247.4', port=5000)
+    app.run(host='127.0.0.1', port=5000)
